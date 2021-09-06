82ffe912772922fba2a3387f8ff04cfe7f7a7335
Shade121 <80048510+Shade121@users.noreply.github.com>
2021-09-06 15:29:34 +0300

    changed post

--- src/mailer/smart.php
+++ src/mailer/smart.php
@@ -13,12 +13,12 @@ $mail->CharSet = 'utf-8';
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'v.v.lobosok@gmail.com';                 // Наш логин
-$mail->Password = 'RjhjkmbIen121';                           // Наш пароль от ящика
+$mail->Username = 'postmailler04@gmail.com';                 // Наш логин
+$mail->Password = 'fufnfrhbcnb121';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
-$mail->setFrom('v.v.lobosok@gmail.com', 'Pulse');   // От кого письмо 
+$mail->setFrom('postmailler04@gmail.com', 'Pulse');   // От кого письмо 
 $mail->addAddress('vladimir5093@ukr.net');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
